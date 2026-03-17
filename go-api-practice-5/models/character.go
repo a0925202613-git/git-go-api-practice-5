f5013526ccbda961d41dcc9c7970920c35f1771f
cuzsor <a0925202613@gmail.com>
2026-03-17 22:03:21 +0800

    _

--- go-api-practice-5/models/character.go
+++ go-api-practice-5/models/character.go
@@ -5,8 +5,8 @@ import "time"
 // Character 三麗鷗角色（關聯最愛周邊）
 type Character struct {
 	ID            int       `json:"id"`
-	Name          string    `json:"name" binding:"required"`
-	MerchandiseID int       `json:"merchandise_id" binding:"required"`
+	Name          string    `json:"name"`
+	MerchandiseID int       `json:"merchandise_id"`
 	Intro         string    `json:"intro"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
